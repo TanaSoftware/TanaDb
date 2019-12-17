63a025817621349c1bcef3753c3e781a05ea1a59
TanaSoftware <48710783+TanaSoftware@users.noreply.github.com>
2019-12-17 16:40:30 +0200

    Add files via upload

--- tana.js
+++ tana.js
@@ -1,4 +1,7 @@
-﻿
+﻿String.prototype.replaceAll = function (search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
 
 
 var cScriptLoader = (function () {
@@ -86,27 +89,53 @@ function init() {
 
     GetSources();
 }
-function SetSource(id, source) {
+function SetSource(id, source,params) {
     var elem = document.getElementById(id);
     elem.setAttribute("source", source);
     if (elem.firstChild == null)
         return;
     var InHtml = elem.firstChild.innerHTML;
+    if (InHtml==null)
+        InHtml = elem.innerHTML
+    if (params != null) {
+        if (typeof params =="string" && typeof window[params] == "object")
+            params = window[params];
 
-    GetRest(source, function successGet(data) {
-        
-        var template = "<div style='display:none'>" + InHtml + "</div>"
-        if (data == null || data.length == 0) {
-            elem.innerHTML = template;
-            return;
-        }
+        PostRest(source, params, function successGet(data) {
+            if (data == null || data.length == 0) {
+                elem.innerHTML = "<div style='display:none'>" + InHtml + "</div>";
+                return;
+            }
+            var htm = getDigestHtml(data, InHtml);
 
-        var htm = getDigestHtml(data, InHtml);
+            if (htm != "") {
+                elem.innerHTML = SetDivWrapper(InHtml, htm);
+                elem.style.display = "block";
+            }
+            else
+                elem.innerHTML = "<div style='display:none'>" + InHtml + "</div>";
+        }, errorGet);
+    }
+    else {
+        GetRest(source, function successGet(data) {
 
-        //var templateId = "dvTemplate__" + i;
-        if (htm != "")
-            elem.innerHTML = template + htm;
-    }, errorGet);
+            var template = InHtml;
+            if (data == null || data.length == 0) {
+                elem.innerHTML = "<div style='display:none'>" + InHtml + "</div>";
+                return;
+            }
+
+            var htm = getDigestHtml(data, InHtml);
+
+            //var templateId = "dvTemplate__" + i;
+            if (htm != "") {
+                elem.innerHTML = SetDivWrapper(template, htm);
+                elem.style.display = "block";
+            }
+            else
+                elem.innerHTML = "<div style='display:none'>" + InHtml + "</div>";
+        }, errorGet);
+    }
 }
 
 function loadScript(path, callback) {
@@ -144,53 +173,98 @@ function loadScript(path, callback) {
         }
     }
 }
-function build(template,data, targetId) {
-    var htm = getDigestHtml(data, template);
-    document.getElementById(targetId).innerHTML = htm;
+function render(data, targetId, objName) {
+    var obj = document.getElementById(targetId);
+    if (obj == null)
+        return;
+    var template = obj.childNodes[0].tagName  == "DIV" ? obj.childNodes[0].innerHTML : obj.innerHTML;
+    var htm = getDigestHtml(data, template, objName);
+    obj.innerHTML = SetDivWrapper(template, htm);
+    obj.style.display = "block";
 }
 
-function getDigestHtml(data, InHtml) {
+function getDigestHtml(data, InHtml,objName) {
     var htm = "";
-    for (var j = 0; j < data.length; j++) {
-        var rowData = data[j];
-        var replaceMe = InHtml;
+    if (typeof data == "object") {
+        if (data.length == undefined) {
+            var replaceMe = InHtml;
 
-        for (var r in rowData) {
-            replaceMe = replaceMe.replaceAll("{{" + r + "}}", rowData[r]);
+            for (var r in data) {
+                replaceMe = replaceMe.replaceAll("{{" + r + "}}", data[r]);
+                //replaceMe = replaceMe.replace("[ModelBind]", "onBlur=alert('" + r + "')");
+            }
+            htm += replaceMe;
         }
+        else {
+            for (var j = 0; j < data.length; j++) {
+                var rowData = data[j];
+                var replaceMe = InHtml;
+
+                for (var r in rowData) {
+                    replaceMe = replaceMe.replaceAll("{{" + r + "}}", rowData[r]);
+                }
+                htm += replaceMe;
+            }
+        }
+    }
+    else {
+        var replaceMe = InHtml;
+        replaceMe = replaceMe.replaceAll("{{" + objName + "}}", data);
         htm += replaceMe;
     }
     return htm;
 }
 
-function GetSources() {
-    var scripts = [];
-    var allSources = document.querySelectorAll('[source]');
-    var clientSources = document.querySelectorAll('[clientSource]');
+function SetDivWrapper(template, htm) {
+    return "<div style='display:none'>" + template + "</div>" + htm;
+}
 
-    //clientSource
+function getclientSources(clientSources, scripts) {
     for (var i = 0; i < clientSources.length; i++) {
         clientSources[i].style.display = "none";
         var template = clientSources[i].innerHTML;
-        var url = clientSources[i].getAttribute("clientSource");
-        if (url != null) {
-            var arrUrl = url.split('/');
-            var func = arrUrl[2];
-            var param = arrUrl.length > 3 ? arrUrl[3]:"";
-            loadScript(arrUrl[0]+"/"+arrUrl[1]+".js",function callBack(data,succes){
-                if (succes=="ok") {
-                    var fn = window[func];
-                    if(param.length==0)
-                        fn(template);
-                    else
-                        fn(template, param);
+        var module = clientSources[i].getAttribute("module");
+        if (module != null) {
+            scripts.push(module);
+        }
+        var obj = clientSources[i].getAttribute("clientSource");        
+
+        
+        if (obj != null && obj != "") {
+            var htm = "";
+            if (obj.indexOf('/') < 0) {
+                if (typeof window[obj] == "object") {
+                    htm = getDigestHtml(window[obj], template);
+
                 }
+                else {
+                    htm = getDigestHtml(window[obj], template, obj);
+
+                }
+                clientSources[i].innerHTML = SetDivWrapper(template, htm);
+                clientSources[i].style.display = "block";
                 
-            });                        
+            }
+            else {
+                var arrUrl = obj.split('/');
+                var func = arrUrl[2];
+                var param = arrUrl.length > 3 ? arrUrl[3] : "";
+                loadScript(arrUrl[0] + "/" + arrUrl[1] + ".js", function callBack(data, succes) {
+                    if (succes == "ok") {
+                        var fn = window[func];
+                        if (param.length == 0)
+                            fn(template);
+                        else
+                            fn(template, param);
+                    }
+
+                });
+            }
         }
     }
+}
 
-    //source - server
+function getServerallSources(allSources, scripts) {
     for (var i = 0; i < allSources.length; i++) {
         allSources[i].style.display = "none";
         var template = allSources[i].innerHTML;
@@ -202,29 +276,69 @@ function GetSources() {
         ///get data source from server
         var url = allSources[i].getAttribute("source");
         var fn = window[url];
+        var params = allSources[i].getAttribute("params");
 
         // is object a function?
         if (typeof fn === "function") {
             fn(template, allSources[i]);
         }
         else {
-            if (url != null) {
+            if (url != null && url != "") {
                 var InHtml = allSources[i].innerHTML;
                 (function (i, InHtml, url) {
-                    GetRest(url, function successGet(data) {
-                        var htm = getDigestHtml(data,InHtml);
-                       
-                        allSources[i].style.display = "block";
+                    if (params != null && params != "") {//post
+                        if (typeof window[params] == "object")
+                            params = window[params];
 
-                        var templateId = "dvTemplate__" + i;
-                        allSources[i].innerHTML = "<div id='" + templateId + "' style='display:none'>" + template + "</div>" + htm;
-                    }, errorGet);
+                        PostRest(url, params, function successGet(data) {
+                            var htm = getDigestHtml(data, InHtml);
+
+                            allSources[i].style.display = "block";
+
+                            allSources[i].innerHTML = "<div style='display:none'>" + template + "</div>" + htm;
+                        }, errorGet);
+                    }
+                    else {//get
+                        GetRest(url, function successGet(data) {
+                            var htm = getDigestHtml(data, InHtml);
+
+                            allSources[i].style.display = "block";
+                            
+                            allSources[i].innerHTML = "<div style='display:none'>" + template + "</div>" + htm;
+                        }, errorGet);
+                    }
                 }).call(this, i, InHtml, url);
             }
         }
     }
-    var ScriptLoader = new cScriptLoader(scripts);
-    ScriptLoader.loadFiles();
+}
+function GetSources() {
+    //var xBody = document.getElementsByTagName("BODY")[0];
+    //xBody.style.display = "none";
+    var scripts = [];
+    var allSources = document.querySelectorAll('[source]');
+    var clientSources = document.querySelectorAll('[clientSource]');
+    var clientSourcesTempates = document.querySelectorAll('template');
+
+    //clientSourcesTempates
+    for (var i = 0; i < clientSourcesTempates.length; i++) {
+        var temp = document.querySelector(document.querySelectorAll('template')[i].id).shadowRoot.querySelectorAll("[clientSource]");
+        getclientSources(temp, scripts);
+        var temp2 = document.querySelector(document.querySelectorAll('template')[i].id).shadowRoot.querySelectorAll("[source]");
+        getServerallSources(temp2, scripts);
+    }
+
+    //clientSource
+    getclientSources(clientSources, scripts);
+
+    //source - server
+    getServerallSources(allSources, scripts);
+
+    //xBody.style.display = "block";
+    if (scripts != null && scripts.length > 0) {
+        var ScriptLoader = new cScriptLoader(scripts);
+        ScriptLoader.loadFiles();
+    }
 }
 
 function onLoadJs(data) {
@@ -240,27 +354,52 @@ function errorGet(data,url) {
     
 }
 
-String.prototype.replaceAll = function (search, replacement) {
-    var target = this;
-    return target.replace(new RegExp(search, 'g'), replacement);
-};
+
 
 function GetRest(url, success, error) {
     var req = new XMLHttpRequest();
     req.onreadystatechange = function () {
         if (this.readyState == 4) {
+            
             if (req.status != 200 && req.status != 304) {
                 error(this.responseText);
                 return;
             }
-            var data = JSON.parse(this.responseText);
-            success(data);
-
+            if (req.status == 200) {                
+                var data = JSON.parse(this.responseText);
+                success(data);
+            }
 
         }
     }
     req.open('GET', url, true);
     req.send();
+
+}
+
+function PostRest(url,params, success, error) {
+    var req = new XMLHttpRequest();
+    
+    req.onreadystatechange = function () {
+        if (this.readyState == 4) {
+
+            if (req.status != 200 && req.status != 304) {
+                error(this.responseText);
+                return;
+            }
+            if (req.status == 200) {
+                var data = JSON.parse(this.responseText);
+                success(data);
+            }
+
+        }
+    }
+    req.open('POST', url, true);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    if (typeof params == "string")
+        req.send(params);
+    else
+        req.send(JSON.stringify(params));
 
 }
 
@@ -285,7 +424,5 @@ function loadJS(url, onDone, onError) {
         onError(e);
     }
 }
-
-
 
 
